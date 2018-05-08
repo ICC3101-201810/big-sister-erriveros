bbb63403db49ab168bb3a587f5f809a3f2043d50
erriveros <38014189+erriveros@users.noreply.github.com>
2018-05-08 13:54:05 -0700

    Add files via upload

---
+++ big-sister-erriveros-master/LabPOO/EventPrueba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPOO
+{
+    public class EventPrueba
+    {
+
+        public event TickHandler Tick;
+        public EventArgs e = null;
+        public delegate void TickHandler(EventPrueba ev, EventArgs e);
+
+    }
+}
